26276a1219f99c3ff3139cc72f780630b0e294c7
Joe-muraguri <joe.crrm@gmail.com>
2024-12-30 15:46:55 +0300

    port binding for render

--- app.py
+++ app.py
@@ -117,5 +117,9 @@ def sendStkPush(phone_number):
         print('Error:', str(e))
 
 
+import os
 
-app.run(debug=True)
+
+ # Get the port from the environment variable or default to 5000
+port = int(os.environ.get("PORT", 5000))
+app.run(debug=True, host='0.0.0.0', port=port)
